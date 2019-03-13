47c7bb333a372f6346bcef5b626286ed1e9a0946
mcarso15 <mcarso15@uncc.edu>
2019-03-13 17:39:45 -0400

    Implemented HealthBar HUD and damage system

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpHeight = 5f;
+    [SerializeField] private float mouseHorizSens = 6f;
+    [SerializeField] private float mouseVertSens = 8f;
+    [SerializeField] private float fallMultiplier = 2f;
+
+    [SerializeField] private float damageMultiplier = 0.1f;
+
+    
+    private float healthWidth;
+    private float currHealthWidth;
+    private float currHealth;
+    private float damageValue;
+    private Camera cam;
+
+    private bool grounded;
+
+    GameObject healthBar;
+
+    RectTransform healthTransform;
+    
+
+    private PlayerMovement movement;
+
+    void Start(){
+        movement = GetComponent<PlayerMovement>();
+        //Get the width of the health bar
+        healthBar = GameObject.FindWithTag("HealthBar");
+        healthTransform = healthBar.GetComponent<RectTransform>();
+        healthWidth = healthTransform.rect.width;
+        currHealthWidth = healthWidth;
+
+        damageValue = (healthWidth * damageMultiplier);
+    }
+
+    void Update(){
+        DoMovement();
+
+        TestHealth();
+    }
+
+    void DoMovement(){
+        float xMovement = Input.GetAxisRaw("Horizontal"); //-1 to 1
+        float zMovement = Input.GetAxisRaw("Vertical"); //-1 to 1
+
+        Vector3 movHoriz = transform.right * xMovement;
+        Vector3 movVert = transform.forward * zMovement;
+
+        Vector3 velocity = (movHoriz + movVert).normalized * speed;
+
+        movement.Move(velocity);
+
+        //Calculate rotation. Only want 'Y' rotation on player. 'Turning'.
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        
+        Vector3 rotation = new Vector3(0f, yRotation, 0f) * mouseHorizSens;
+
+        movement.Rotate(rotation);
+
+        //Calculate camera rotation
+        float xRotation = Input.GetAxisRaw("Mouse Y") ;
+        Vector3 camRotation = new Vector3(xRotation, 0f, 0f)* mouseVertSens;
+
+        movement.CamRotate(camRotation);
+
+        if(grounded){
+            if(Input.GetButtonDown("Jump")){
+                movement.Jump(jumpHeight);
+                movement.FallMulti(fallMultiplier);
+                movement.DoJump();
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.tag == "Ground"){
+            grounded = true;
+        }
+
+        if(other.gameObject.tag == "Projectile"){
+            Destroy(other.gameObject);
+            healthTransform.sizeDelta = new Vector2(currHealthWidth - damageValue, healthTransform.sizeDelta.y);
+            currHealthWidth -= damageValue;
+        }
+    }
+
+    void OnCollisionExit(Collision other){
+        if(other.gameObject.tag == "Ground"){
+            grounded = false;
+        }
+    }
+
+    void TestHealth(){
+        if(Input.GetKeyDown(KeyCode.E)){
+            healthTransform.sizeDelta = new Vector2(currHealthWidth - damageValue, healthTransform.sizeDelta.y);
+            currHealthWidth -= damageValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            if(currHealthWidth < healthWidth){
+                healthTransform.sizeDelta = new Vector2(currHealthWidth + damageValue, healthTransform.sizeDelta.y);
+                currHealthWidth += damageValue;
+            }
+        }
+    }
+}
