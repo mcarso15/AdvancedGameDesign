85a42f8e7f98f206b3f2cc48588c5a715e76d406
mcarso15 <mcarso15@uncc.edu>
2019-03-14 01:36:04 -0400

    Implemented enemy chase mechanic

--- New Unity Project/Assets/Scripts/EnemyController.cs
+++ New Unity Project/Assets/Scripts/EnemyController.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private GameObject pooPrefab;
+    [SerializeField] private float minDistance = 1f;
+    [SerializeField] private float maxDistance = 3f;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float maxFollowFrom = 10f;
+
+    [SerializeField] private float turnAngle = 15f;
+    [SerializeField] private float tryAgainDist = 5f;
     private Rigidbody rb;
     
     private Collider enemyCollider;
@@ -12,25 +19,32 @@ public class EnemyController : MonoBehaviour
     private GameObject player;
     private float distanceToPlayer;
 
+    private float _tryAgainDist = 0f;
+
     private Vector3 playerDirection;
+    private Vector3 _rotation;
+
+    private Vector3 oldPosition;
+
+    private RaycastHit hit;
+
+    private bool inAWall;
 
     private bool dead;
+
+    private bool hitDetect;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         enemyCollider = GetComponent<Collider>();
         player = GameObject.FindWithTag("Player");
         dead = false;
+
+        OrientEnemies();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        playerDirection = player.transform.position - this.transform.position + Vector3.up;
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(playerDirection), 1);
-        
-    }
     void FixedUpdate(){
+        FollowPlayer();
         TestFight();
     }
 
@@ -47,15 +61,72 @@ public class EnemyController : MonoBehaviour
 
             dead = true;
         }
+
+        if(other.gameObject.tag == "Wall"){
+            inAWall = true;
+        }
         
     }
 
+    void OnCollisionExit(Collision other){
+        if(other.gameObject.tag == "Wall"){
+            inAWall = false;
+        }
+    }
+
+
+    void FollowPlayer(){
+        if(!inAWall && _tryAgainDist <= 0){
+            //Make sure enemy is 'looking' at the player so they can aim
+            playerDirection = player.transform.position - this.transform.position + Vector3.up;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(playerDirection), 1);
+        }
+        //Move towards the player
+        if(Vector3.Distance(transform.position, player.transform.position) >= minDistance && Vector3.Distance(transform.position, player.transform.position) < maxFollowFrom){
+            //Check if I'm crashing into a wall, then determine which way to turn
+            if(inAWall){
+                _tryAgainDist = tryAgainDist;
+                //If moving right makes me closer than moving straight or left, turn right, then move straight.
+                if(Vector3.Distance(transform.position + Vector3.right, player.transform.position) <= Vector3.Distance(transform.position - Vector3.right, player.transform.position)){
+                    _rotation = new Vector3(0f, turnAngle, 0f);
+                    rb.MoveRotation(transform.rotation * Quaternion.Euler(_rotation));
+                    transform.position += transform.right * moveSpeed * Time.fixedDeltaTime;
+                    
+                }
+                //If moving left makes me closer than moving straight or right, turn left, then move straight.
+                if(Vector3.Distance(transform.position - Vector3.right, player.transform.position) < Vector3.Distance(transform.position + Vector3.right, player.transform.position)){
+                    _rotation = new Vector3(0f, -turnAngle, 0f);
+                    rb.MoveRotation(transform.rotation * Quaternion.Euler(_rotation));
+                    transform.position -= transform.right * moveSpeed * Time.fixedDeltaTime;
+                }
+            }
+            
+            else{
+                transform.position += transform.forward * moveSpeed * Time.fixedDeltaTime;
+            }
+            _tryAgainDist--;
+        }
+
+        if(Vector3.Distance(transform.position, player.transform.position) <= maxDistance){
+            print("boo!");
+        }
+        
+    }
     void TestFight(){
         if(Input.GetKeyDown(KeyCode.Q)){
-            GameObject pooProjectile = (GameObject)Instantiate(pooPrefab, transform.position, transform.rotation);
-            Physics.IgnoreCollision(pooProjectile.GetComponent<Collider>(), GetComponent<Collider>());
-            float force = pooProjectile.GetComponent<EnemyProjectileController>().speed;
-            pooProjectile.GetComponent<Rigidbody>().AddForce(pooProjectile.transform.forward * force, ForceMode.Impulse);
+            FireProjectile();
         }
+    }
+
+    void FireProjectile(){
+        GameObject pooProjectile = (GameObject)Instantiate(pooPrefab, transform.position, transform.rotation);
+        Physics.IgnoreCollision(pooProjectile.GetComponent<Collider>(), GetComponent<Collider>());
+        float force = pooProjectile.GetComponent<EnemyProjectileController>().speed;
+        pooProjectile.GetComponent<Rigidbody>().AddForce(pooProjectile.transform.forward * force, ForceMode.Impulse);
+    }
+
+    void OrientEnemies(){
+        playerDirection = player.transform.position - this.transform.position + Vector3.up;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(playerDirection), 1);
     }
 }
