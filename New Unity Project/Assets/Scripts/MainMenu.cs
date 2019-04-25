f02b16d903261c8e4211d56210c59e38b32ce6e1
Sarah Schill <sschill@uncc.edu>
2019-04-25 14:42:42 -0400

    Added things

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject FloatingTextPrefab; 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayTutorial()
+    {
+        //play the introLevel
+        SceneManager.LoadScene(2); 
+
+        //Trigger floating text to explain game.
+
+
+
+
+    }
+
+
+}
