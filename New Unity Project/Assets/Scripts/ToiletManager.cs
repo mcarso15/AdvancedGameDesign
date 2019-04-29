adde7d0d8f352b8a59740c02681e51209328664f
Sarah Schill <sschill@uncc.edu>
2019-04-29 19:37:08 -0400

    added different enemies

--- New Unity Project/Assets/Scripts/ToiletManager.cs
+++ New Unity Project/Assets/Scripts/ToiletManager.cs
@@ -8,13 +8,19 @@ public class ToiletManager : MonoBehaviour
      *  This is where the scripts will be when the toilets need to
      *  change colors, AKA when there is a collision with the toilet
      *  and the plunger gun   
-     */   
+     */
 
+    private Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         //toilet is red... FF0000
+        //Fetch the Renderer from the GameObject
+
+        //Set the main Color of the Material to green
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", Color.green);
     }
 
     // Update is called once per frame
@@ -23,8 +29,13 @@ public class ToiletManager : MonoBehaviour
         
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionEnter(Collision other)
     {
-        //toilet turns white... FFFFFF
+        if (other.gameObject.tag == "Projectile")
+        {
+            //Set the main Color of the Material to red
+            rend.material.shader = Shader.Find("_Color");
+            rend.material.SetColor("_Color", Color.red);
+        }
     }
 }
