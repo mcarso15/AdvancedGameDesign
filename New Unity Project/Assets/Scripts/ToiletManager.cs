8c4c9881a599fb43e6885eb9d1050a089caee5fd
Sarah Schill <sschill@uncc.edu>
2019-04-29 17:25:14 -0400

    worked on tutorial

---
+++ New Unity Project/Assets/Scripts/ToiletManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToiletManager : MonoBehaviour
+{
+    /*
+     *  This is where the scripts will be when the toilets need to
+     *  change colors, AKA when there is a collision with the toilet
+     *  and the plunger gun   
+     */   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //toilet is red... FF0000
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //toilet turns white... FFFFFF
+    }
+}
